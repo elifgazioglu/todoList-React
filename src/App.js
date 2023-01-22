2f5505fc430e97a9210016281487a0f14c18a3d9
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-01-22 23:08:36 +0300

    add delete and select button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import {} from "reactstrap";
 import Todo from "./components/Todos";
 import { useState } from "react";
+import { AiFillDelete } from "react-icons/ai";
+import { GrCheckbox, GrCheckboxSelected } from "react-icons/gr";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -20,19 +22,23 @@ function App() {
     setMessage("");
   }
 
+  function todoSelected() {
+    <GrCheckboxSelected></GrCheckboxSelected>;
+  }
+
   return (
     <div id="container">
       <h1 id="my-todo-list">My Todo List</h1>
-        <div id="container-input">
+      <div id="container-input">
         <input
-        id="input"
+          id="input"
           type="text"
           placeholder="Your Todos"
           onChange={handleChange}
           value={message}
         ></input>
         <button
-          id="btn"
+          id="btn-add"
           type="submit"
           onClick={(event) => {
             addToTodos(message);
@@ -42,12 +48,22 @@ function App() {
         >
           Ekle
         </button>
-        </div>
-        <div id="todo-list">
+      </div>
+      <div id="todo-list">
         {todos.map((todo) => (
-          <Todo todo={todo}> </Todo>
+          <div className="todo-item">
+            <button className="check-btn">
+              <GrCheckbox></GrCheckbox>
+            </button>
+
+            <Todo todo={todo}></Todo>
+            <button id="delete-btn">
+              {" "}
+              <AiFillDelete></AiFillDelete>
+            </button>
+          </div>
         ))}
-        </div>
+      </div>
     </div>
   );
 }
